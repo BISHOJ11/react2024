912b6d46c7b5c778a6eb9f1d1e3d40a2d8af4869
BISHOJ BASNET <basnet4life@gmail.com>
2024-10-24 22:28:17 +0545

    First React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>React is on</h1>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>Vite is on </h1>
+  )
+}
+
+export default App
